4bb8ef06aa1f69fab78d649c0c01c6a6aa0fe2fd
arvind singharpuria <arvind.020606@gmail.com>
2025-12-07 21:38:22 +0530

    05: ai jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,25 @@
+import { openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+    const summarizer = createAgent({
+      name: "summarizer",
+      system: "You are an expert summarizer, You summarizer in 2 words.",
+      model: openai({model: "gpt-4o"}),
+    })
+
+    const { output } =  await summarizer.run(
+      `Summarize the following text: ${event.data.value}`
+    )
+
+    // console.log(output);
+
+    // await step.sleep("wait-a-moment", "5s");
+    // return { message: `Hello ${event.data.value}!` };
+    return { output}
   },
 );
