72ecc804e28d3b937d4ec177c347c56b7a7d8230
arvind singharpuria <arvind.020606@gmail.com>
2025-12-08 16:21:07 +0530

    06: e2b sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,4 +1,6 @@
+import { Sandbox } from "@e2b/code-interpreter";
 import { openai, createAgent } from "@inngest/agent-kit";
+import { getSandbox } from "@/inngest/utils";
 
 import { inngest } from "./client";
 
@@ -6,20 +8,42 @@ export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    const summarizer = createAgent({
-      name: "summarizer",
-      system: "You are an expert summarizer, You summarizer in 2 words.",
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-test-arvind");
+      return sandbox.sandboxId;
+    })
+    
+    // const summarizer = createAgent({
+    //   name: "summarizer",
+    //   system: "You are an expert summarizer, You summarizer in 2 words.",
+    //   model: openai({model: "gpt-4o"}),
+    // })
+
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer. You can write readable and maintainable code. You write simple Next.js and React snippets.",
       model: openai({model: "gpt-4o"}),
     })
 
-    const { output } =  await summarizer.run(
-      `Summarize the following text: ${event.data.value}`
+    // const { output } =  await codeAgent.run(
+    //   `Summarize the following text: ${event.data.value}`
+    // )
+
+    const { output } =  await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
     )
 
     // console.log(output);
 
     // await step.sleep("wait-a-moment", "5s");
     // return { message: `Hello ${event.data.value}!` };
-    return { output}
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000)
+      return `http://${host}`
+    })
+    
+    return { output, sandboxUrl }
   },
 );
